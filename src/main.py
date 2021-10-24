5b0bcf63a50c856c910ffbc4cdf5435db6801c0b
Michiel Vanderhoydonck <10730699+Arphylion@users.noreply.github.com>
2021-10-24 18:19:30 +0200

    feat: start connection to local server based on environment vars

--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
 import typer
+from plexapi.server import PlexServer
+from os import getenv
+
+
+def init_plex():
+    baseurl = getenv("PLEX_BASE_URL", None)
+    token = getenv("PLEX_TOKEN", None)
+
+    if not token:
+        typer.secho("PLEX_TOKEN not defined.", fg=typer.colors.RED, bold=True)
+        raise typer.Exit()
+    if not baseurl:
+        typer.secho("PLEX_BASE_URL not defined.", fg=typer.colors.RED, bold=True)
+        raise typer.Exit()
+
+    plex = PlexServer(baseurl, token)
+    typer.secho(
+        f"Connected to {plex.friendlyName} (version: {plex.version})",
+        fg=typer.colors.YELLOW,
+        bold=True,
+    )
+    if not plex.isLatest():
+        typer.secho(
+            f"Server out of date, consider updating!", fg=typer.colors.RED, bold=True
+        )
+
+    return plex
 
 
 def main():
-    typer.echo("plex-tools")
+    init_plex()
 
 
 if __name__ == "__main__":
